8141165abce25aa9737360307cfb9066223b4c58
vincentiusdaryl <6181901023@student.unpar.ac.id>
2022-11-30 14:25:11 +0700

    Fix bug ModulRPL.java

--- ModulRPL.java
+++ ModulRPL.java
@@ -4,15 +4,15 @@ public class ModulRPL {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        
         System.out.println("Masukkan bobot nilai dari dosen Pembimbing:");
-        int bobotPembimbing = sc.nextInt();
+        Double bobotPembimbing = sc.nextDouble();
 
         System.out.println("Masukkan bobot nilai dari dosen Penguji 1:");
-        int bobotPenguji1 = sc.nextInt();
+        Double bobotPenguji1 = sc.nextDouble();
 
         System.out.println("Masukkan bobot nilai dari dosen Penguji 2:");
-        int bobotPenguji2 = sc.nextInt();
+        Double bobotPenguji2 = sc.nextDouble();
 
         System.out.println("Masukkan nilai dari dosen Pembimbing");
         Double nilaiPembimbing = sc.nextDouble();
@@ -27,8 +27,9 @@ public class ModulRPL {
         Double nilaiAkhirPenguji1 = (bobotPenguji1 / 100) * nilaiPenguji1;
         Double nilaiAkhirPenguji2 = (bobotPenguji2 / 100) * nilaiPenguji2;
 
-        System.out.println("Nilai Akhir Skripsi Mahasiswa:");
+        System.out.println("Nilai Akhir Skripsi Mahasiswa: ");
         Double nilaiAkhirMahasiswa = nilaiAkhirPembimbing + nilaiAkhirPenguji1 + nilaiAkhirPenguji2;
+        System.out.print(nilaiAkhirMahasiswa);
 
 
     }
