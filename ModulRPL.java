c29519cb15fd32ff93ae0b399d17bd8dfed6e106
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-11-30 14:40:39 +0700

    modifikasi java

--- ModulRPL.java
+++ ModulRPL.java
@@ -4,15 +4,26 @@ public class ModulRPL {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Masukkan bobot nilai dari dosen Pembimbing:");
-        Double bobotPembimbing = sc.nextDouble();
 
-        System.out.println("Masukkan bobot nilai dari dosen Penguji 1:");
-        Double bobotPenguji1 = sc.nextDouble();
+        System.out.println("Masukkan Jumlah Komponen Penilaian");
+        int jumlahKomponen = sc.nextInt();
+        String[] komponenPenilaian = new String[jumlahKomponen];
+        double[] bobotPenilaian = new double[3];
 
-        System.out.println("Masukkan bobot nilai dari dosen Penguji 2:");
-        Double bobotPenguji2 = sc.nextDouble();
+        for(int i = 0;i<jumlahKomponen;i++){
+            System.out.printf("Masukkan komponen penilaian %d \n",i);
+            komponenPenilaian[i] = sc.nextLine();
+        }
+        for(int i = 0;i<3;i++){
+            if(i<2){
+                System.out.printf("Masukkan bobot penilaian dosen penguji %d \n",i);
+                bobotPenilaian[i] = sc.nextDouble();
+            }
+            else{
+                System.out.println("Masukkan bobot penilaian dosen pembimbing");
+                bobotPenilaian[i] = sc.nextDouble();
+            }
+        }
 
         System.out.println("Masukkan nilai dari dosen Pembimbing");
         Double nilaiPembimbing = sc.nextDouble();
@@ -23,9 +34,9 @@ public class ModulRPL {
         System.out.println("Masukkan nilai dari dosen Penguji 2:");
         Double nilaiPenguji2 = sc.nextDouble();
 
-        Double nilaiAkhirPembimbing = (bobotPembimbing / 100) * nilaiPembimbing;
-        Double nilaiAkhirPenguji1 = (bobotPenguji1 / 100) * nilaiPenguji1;
-        Double nilaiAkhirPenguji2 = (bobotPenguji2 / 100) * nilaiPenguji2;
+        Double nilaiAkhirPembimbing = (bobotPenilaian[2] / 100) * nilaiPembimbing;
+        Double nilaiAkhirPenguji1 = (bobotPenilaian[0] / 100) * nilaiPenguji1;
+        Double nilaiAkhirPenguji2 = (bobotPenilaian[1] / 100) * nilaiPenguji2;
 
         System.out.println("Nilai Akhir Skripsi Mahasiswa: ");
         Double nilaiAkhirMahasiswa = nilaiAkhirPembimbing + nilaiAkhirPenguji1 + nilaiAkhirPenguji2;
