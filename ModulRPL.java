7951f14d15c50956b2b6a0637d959c20cbb7e02c
MichaelAlexanderJ <Michaelalex2705@gmail.com>
2022-11-30 17:47:43 +0700

    perbaikin java

--- ModulRPL.java
+++ ModulRPL.java
@@ -8,35 +8,44 @@ public class ModulRPL {
         System.out.println("Masukkan Jumlah Komponen Penilaian");
         int jumlahKomponen = sc.nextInt();
         String[] komponenPenilaian = new String[jumlahKomponen];
-        double[] bobotPenilaian = new double[3];
+        double[] nilaiPenguji1 = new double[jumlahKomponen];
+        double[] nilaiPenguji2 = new double[jumlahKomponen];
+        double[] nilaiPembimbing = new double[jumlahKomponen];
 
-        for(int i = 0;i<jumlahKomponen;i++){
+        for(int i = 1;i<=jumlahKomponen;i++){
             System.out.printf("Masukkan komponen penilaian %d \n",i);
-            komponenPenilaian[i] = sc.nextLine();
+            komponenPenilaian[i] = sc.next();
         }
-        for(int i = 0;i<3;i++){
-            if(i<2){
-                System.out.printf("Masukkan bobot penilaian dosen penguji %d \n",i);
-                bobotPenilaian[i] = sc.nextDouble();
-            }
-            else{
+                System.out.printf("Masukkan bobot penilaian dosen penguji \n");
+                double bobotPenilaianDosbing = sc.nextDouble();
+
                 System.out.println("Masukkan bobot penilaian dosen pembimbing");
-                bobotPenilaian[i] = sc.nextDouble();
-            }
+                double bobotPenilaianDosenPenguji = sc.nextDouble();
+            
+        
+        
+        Double nilaiAkhirPembimbing = (double) 0;
+        Double nilaiAkhirPenguji1 = (double) 0;
+        Double nilaiAkhirPenguji2 = (double) 0;
+
+        for(int i = 1;i<=jumlahKomponen;i++){
+            System.out.printf("Masukkan nilai komponen penilaian %s untuk dosen pembimbing",komponenPenilaian[i]);
+            nilaiPembimbing[i] = sc.nextDouble();
+            System.out.printf("Masukkan nilai komponen penilaian %s untuk dosen penguji 1",komponenPenilaian[i]);
+            nilaiPenguji1[i] = sc.nextDouble();
+            System.out.printf("Masukkan nilai komponen penilaian %s untuk dosen penguji 2",komponenPenilaian[i]);
+            nilaiPenguji2[i] = sc.nextDouble();
+            nilaiAkhirPembimbing+=nilaiPembimbing[i];
+            nilaiAkhirPenguji1+=nilaiPenguji1[i];
+            nilaiAkhirPenguji2+=nilaiPenguji2[i];
         }
+        nilaiAkhirPembimbing = nilaiAkhirPembimbing/jumlahKomponen;
+        nilaiAkhirPenguji1 = nilaiAkhirPenguji1/jumlahKomponen;
+        nilaiAkhirPenguji2 = nilaiAkhirPenguji2/jumlahKomponen;
 
-        System.out.println("Masukkan nilai dari dosen Pembimbing");
-        Double nilaiPembimbing = sc.nextDouble();
-
-        System.out.println("Masukkan nilai dari dosen Penguji 1:");
-        Double nilaiPenguji1 = sc.nextDouble();
-
-        System.out.println("Masukkan nilai dari dosen Penguji 2:");
-        Double nilaiPenguji2 = sc.nextDouble();
-
-        Double nilaiAkhirPembimbing = (bobotPenilaian[2] / 100) * nilaiPembimbing;
-        Double nilaiAkhirPenguji1 = (bobotPenilaian[0] / 100) * nilaiPenguji1;
-        Double nilaiAkhirPenguji2 = (bobotPenilaian[1] / 100) * nilaiPenguji2;
+        nilaiAkhirPembimbing = nilaiAkhirPembimbing/bobotPenilaianDosbing;
+        nilaiAkhirPenguji1 = nilaiAkhirPembimbing/bobotPenilaianDosenPenguji;
+        nilaiAkhirPenguji2 = nilaiAkhirPenguji2/bobotPenilaianDosenPenguji;
 
         System.out.println("Nilai Akhir Skripsi Mahasiswa: ");
         Double nilaiAkhirMahasiswa = nilaiAkhirPembimbing + nilaiAkhirPenguji1 + nilaiAkhirPenguji2;
